76ec6ceee9273f0dd5c1c56243f4153834ccdd7d
Evgeny Kovito <zag7moon@gmail.com>
2019-07-18 13:17:09 +0700

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './state/store';
 
 import './index.css';
 import TheRouter from './router';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<TheRouter />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <TheRouter />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
